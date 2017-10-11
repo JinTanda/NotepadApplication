4ce7555b794878c4373fd3379a978bf37d49dead
Jack <pochomkingdom@gmail.com>
2017-10-11 14:31:29 +0900

    addGitignore

--- Pods/Target Support Files/Pods-MyFirstApplication/Pods-MyFirstApplication-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyFirstApplication : NSObject
-@end
-@implementation PodsDummy_Pods_MyFirstApplication
-@end
--- Pods/Target Support Files/Pods-MyFirstApplicationTests/Pods-MyFirstApplicationTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyFirstApplicationTests : NSObject
-@end
-@implementation PodsDummy_Pods_MyFirstApplicationTests
-@end
